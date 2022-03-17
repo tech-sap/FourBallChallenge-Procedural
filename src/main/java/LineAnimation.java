c6fdd9845aa5aad1febf5b83c4888034e5c4f14c
iamsk <saravanankmk17@gmail.com>
2022-03-17 10:20:02 +0530

    commited every files in FourBallChallenge procedural attempt

---
+++ src/main/java/LineAnimation.java
@@ -0,0 +1,52 @@
+import processing.core.PApplet;
+
+
+public class LineAnimation extends PApplet {
+
+     public LineAnimation() {
+         PApplet.main("TryProcessingOops");
+     }
+//    public static final float Y_AXIS_FOUR = 400;
+//    public static final float Y_AXIS_THREE = 300;
+//    public static final float Y_AXIS_TWO = 200;
+//    public static final float Y_AXIS_ONE = 100;
+//
+//    private float xAxisFour = 0;
+//    private float xAxisThree = 0;
+//    private float xAxisTwo = 0;
+//    private float xAxisOne = 0;
+//
+//    @Override
+//    public void settings() {
+//        super.settings();
+//        size(700, 500);
+//    }
+//
+//    @Override
+//    public void draw() {
+//        drawLineOne();
+//        drawLineTwo();
+//        drawLineThree();
+//        drawLineFour();
+//    }
+//
+//    private void drawLineFour() {
+//        ellipse(xAxisFour, Y_AXIS_FOUR, DIAMETER, DIAMETER);
+//        xAxisFour += 5.5;
+//    }
+//
+//    private void drawLineThree() {
+//        ellipse(xAxisThree, Y_AXIS_THREE, DIAMETER, DIAMETER);
+//        xAxisThree += 4;
+//    }
+//
+//    private void drawLineTwo() {
+//        ellipse(xAxisTwo, Y_AXIS_TWO, DIAMETER, DIAMETER);
+//        xAxisTwo += 2.5;
+//    }
+//
+//    private void drawLineOne() {
+//        ellipse(xAxisOne, Y_AXIS_ONE, DIAMETER, DIAMETER);
+//        xAxisOne += 1;
+//    }
+}
